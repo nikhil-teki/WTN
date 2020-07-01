068d77957ca4e4243eb3a40bc32e66d8ddf5f6e0
nikhil-teki <nikhil.teki2000@gmail.com>
2020-07-01 18:49:20 +0530

    Interfaces Commit 1

---
+++ Milestone1/src/live/Test.java
@@ -0,0 +1,17 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Playable veena=new Veena();
+		Playable saxophone=new Saxophone();
+		veena.play();
+		saxophone.play();
+	}
+
+}
