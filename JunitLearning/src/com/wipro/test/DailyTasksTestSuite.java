1eb8351818ef60959275d4e43a045f1c8b6eb972
nikhil-teki <nikhil.teki2000@gmail.com>
2020-07-27 19:52:17 +0530

    Milestone 1 Test Suite Commit 1

---
+++ JunitLearning/src/com/wipro/test/DailyTasksTestSuite.java
@@ -0,0 +1,10 @@
+package com.wipro.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TestCheckPresence.class, TestSort.class, TestStringConcat.class })
+public class DailyTasksTestSuite {
+
+}
