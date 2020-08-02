56f21f656f8839ec369ffacbc368c90bb122c694
nikhil-teki <nikhil.teki2000@gmail.com>
2020-08-02 21:52:03 +0530

    Collection List Commit 1

---
+++ Milestone1/src/com/wipro/collection/list/Employee.java
@@ -0,0 +1,13 @@
+package com.wipro.collection.list;
+
+public class Employee {
+	int empId;
+	String empName;
+	String email;
+	String gender;
+	float salary;
+	void getEmployeeDetails()
+	{
+		System.out.println(empId+" "+empName+" "+email+" "+gender+" "+salary);
+	}
+}
---
+++ Milestone1/src/com/wipro/collection/list/Ex4.java
@@ -0,0 +1,31 @@
+package com.wipro.collection.list;
+
+import java.util.ArrayList;
+
+@SuppressWarnings("serial")
+class NumberArrayList<E> extends ArrayList<E>
+{
+	public boolean add(E e)
+	{
+		if((e instanceof Byte)||(e instanceof Integer)||(e instanceof Float)||(e instanceof Double))
+		{
+			return super.add(e);
+		}
+		else
+			return false;
+	}
+}
+public class Ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Object> list=new NumberArrayList<>();
+		System.out.println("Added : "+list.add(Byte.MAX_VALUE));
+		System.out.println("Added : "+list.add(100));
+		System.out.println("Added : "+list.add(3.1415f));
+		System.out.println("Added : "+list.add(3.141592653589793238462643383279));
+		System.out.println("Added : "+list.add("I am a String. May I come in please"));
+		System.out.println("The list : "+list);
+	}
+
+}
